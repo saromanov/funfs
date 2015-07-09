ef1a17e4538e4253d3ff3e0481bb4c6dcb3df062
Sergey Romanov <xxsmotur@gmail.com>
2015-07-10 02:22:43 +0500

    first commit

---
+++ src/funfs.js
@@ -0,0 +1,55 @@
+import fs from "fs";
+import fuse from "fuse-bindings";
+
+export class FunFS{
+
+    mount(path){
+        funfs_mount(path);
+    }
+
+    unmount(path){
+        funfs_unmount(path);
+    }
+}
+
+
+let funfs_mount = function(path){
+     fuse.mount(path, {
+
+        readdir(path, cb) {
+           fs.readdir(path, (err, result) =>{
+               if(err){
+                cb(fuse.errno(err.code));
+                }
+             
+             cb(0, result);
+        });
+
+        },
+
+
+        open(path, permissions, cb){
+            fs.open(path, permissions, (err, result) => {
+                if(err){
+                    cb(fuse.errno(err));
+                }
+
+                cb(0, result);
+            });
+        },
+
+        create(path, cb){
+            fs.open(path, 'w', (err, result) => {
+                
+            });
+        }
+    });
+
+}
+
+let funfs_unmount = function(path){
+    fuse.unmount(path, err => {
+
+    });
+}
+
